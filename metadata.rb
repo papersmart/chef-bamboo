3c8c5f20f4aa2092cfa365b7fbcc429c49fb6d96
Moritz Breit <mail@mobr.de>
2013-10-17 16:34:54 +0200

    Release version 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'moritz.breit@papersmart.de'
 license          'All rights reserved'
 description      'Installs/Configures bamboo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends "apt"
 depends "java"
